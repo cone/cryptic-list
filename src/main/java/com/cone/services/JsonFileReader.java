06c4468bc6252b1003593d0204ab07d6839a10e5
Carlos Gutierrez <coneramu@gmail.com>
2022-02-05 17:37:39 -0600

    Add JsonFileReader service

---
+++ src/main/java/com/cone/services/JsonFileReader.java
@@ -0,0 +1,34 @@
+package com.cone.services;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class JsonFileReader {
+  File jsonFile;
+
+  public JsonFileReader(File jsonFile) {
+    this.jsonFile = jsonFile;
+  }
+
+  public String read() throws IOException {
+    InputStream inputStream = new FileInputStream(jsonFile);
+    return readFromInputStream(inputStream);
+  }
+
+  private String readFromInputStream(InputStream inputStream)
+  throws IOException {
+    StringBuilder resultStringBuilder = new StringBuilder();
+    try (BufferedReader br
+      = new BufferedReader(new InputStreamReader(inputStream))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+            resultStringBuilder.append(line);
+        }
+    }
+    return resultStringBuilder.toString();
+  }
+}
---
+++ src/test/java/com/cone/services/JsonFileReaderTest.java
@@ -0,0 +1,20 @@
+package com.cone.services;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JsonFileReaderTest implements WithAssertions {
+  @Test
+  void testRead() throws IOException {
+    File jsonFile = Paths.get("src/test/resources/test.json").toFile();
+    JsonFileReader service = new JsonFileReader(jsonFile);
+    String content = service.read();
+
+    Assertions.assertEquals("{\"hello\":\"world\"}", content);
+  }
+}
