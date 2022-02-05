70501857d34b6360cfe048a5e5b19691eb7623c6
Carlos Gutierrez <coneramu@gmail.com>
2022-02-05 17:36:19 -0600

    Fix odd indentation in the AESUtils tests

--- src/test/java/com/cone/utils/AESUtilsUnitTest.java
+++ src/test/java/com/cone/utils/AESUtilsUnitTest.java
@@ -23,82 +23,82 @@ import org.junit.jupiter.api.Test;
 
 class AESUtilsUnitTest implements WithAssertions {
 
-    @Test
-    void givenString_whenEncrypt_thenSuccess()
-        throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException,
-        BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
-        // given
-        String input = "anitalavalatina";
-        SecretKey key = AESUtils.generateKey(128);
-        IvParameterSpec ivParameterSpec = AESUtils.generateIv();
-        String algorithm = "AES/CBC/PKCS5Padding";
+  @Test
+  void givenString_whenEncrypt_thenSuccess()
+    throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException,
+    BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
+    // given
+    String input = "anitalavalatina";
+    SecretKey key = AESUtils.generateKey(128);
+    IvParameterSpec ivParameterSpec = AESUtils.generateIv();
+    String algorithm = "AES/CBC/PKCS5Padding";
 
-        // when
-        String cipherText = AESUtils.encrypt(algorithm, input, key, ivParameterSpec);
-        String plainText = AESUtils.decrypt(algorithm, cipherText, key, ivParameterSpec);
+    // when
+    String cipherText = AESUtils.encrypt(algorithm, input, key, ivParameterSpec);
+    String plainText = AESUtils.decrypt(algorithm, cipherText, key, ivParameterSpec);
 
-        // then
-        Assertions.assertEquals(input, plainText);
-    }
- 
-    @Test
-    void givenFile_whenEncrypt_thenSuccess()
-        throws NoSuchAlgorithmException, IOException, IllegalBlockSizeException, InvalidKeyException,
-        BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
-        // given
-        SecretKey key = AESUtils.generateKey(128);
-        String algorithm = "AES/CBC/PKCS5Padding";
-        IvParameterSpec ivParameterSpec = AESUtils.generateIv();
-        File inputFile = Paths.get("src/test/resources/cone.txt")
+    // then
+    Assertions.assertEquals(input, plainText);
+  }
+
+  @Test
+  void givenFile_whenEncrypt_thenSuccess()
+    throws NoSuchAlgorithmException, IOException, IllegalBlockSizeException, InvalidKeyException,
+    BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
+    // given
+    SecretKey key = AESUtils.generateKey(128);
+    String algorithm = "AES/CBC/PKCS5Padding";
+    IvParameterSpec ivParameterSpec = AESUtils.generateIv();
+    File inputFile = Paths.get("src/test/resources/cone.txt")
             .toFile();
-        File encryptedFile = new File("classpath:cone.encrypted");
-        File decryptedFile = new File("document.decrypted");
+    File encryptedFile = new File("classpath:cone.encrypted");
+    File decryptedFile = new File("document.decrypted");
 
-        // when
-        AESUtils.encryptFile(algorithm, key, ivParameterSpec, inputFile, encryptedFile);
-        AESUtils.decryptFile(algorithm, key, ivParameterSpec, encryptedFile, decryptedFile);
+    // when
+    AESUtils.encryptFile(algorithm, key, ivParameterSpec, inputFile, encryptedFile);
+    AESUtils.decryptFile(algorithm, key, ivParameterSpec, encryptedFile, decryptedFile);
 
-        // then
-        assertThat(inputFile).hasSameTextualContentAs(decryptedFile);
-        encryptedFile.delete();
-        decryptedFile.delete();
-    }
+    // then
+    assertThat(inputFile).hasSameTextualContentAs(decryptedFile);
+    encryptedFile.delete();
+    decryptedFile.delete();
+  }
 
-    @Test
-    void givenObject_whenEncrypt_thenSuccess()
-        throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException,
-        InvalidAlgorithmParameterException, NoSuchPaddingException, IOException, BadPaddingException,
-        ClassNotFoundException {
-        // given
-        Credentials credentials = new Credentials("cone@email.com", "qwerty123");
-        SecretKey key = AESUtils.generateKey(128);
-        IvParameterSpec ivParameterSpec = AESUtils.generateIv();
-        String algorithm = "AES/CBC/PKCS5Padding";
+  @Test
+  void givenObject_whenEncrypt_thenSuccess()
+    throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException,
+    InvalidAlgorithmParameterException, NoSuchPaddingException, IOException, BadPaddingException,
+    ClassNotFoundException {
+    // given
+    Credentials credentials = new Credentials("cone@email.com", "qwerty123");
+    SecretKey key = AESUtils.generateKey(128);
+    IvParameterSpec ivParameterSpec = AESUtils.generateIv();
+    String algorithm = "AES/CBC/PKCS5Padding";
 
-        // when
-        SealedObject sealedObject = AESUtils.encryptObject(algorithm, credentials, key, ivParameterSpec);
-        Credentials object = (Credentials) AESUtils.decryptObject(algorithm, sealedObject, key, ivParameterSpec);
+    // when
+    SealedObject sealedObject = AESUtils.encryptObject(algorithm, credentials, key, ivParameterSpec);
+    Credentials object = (Credentials) AESUtils.decryptObject(algorithm, sealedObject, key, ivParameterSpec);
 
-        // then
-        assertThat(credentials).isEqualTo(object);
-    }
+    // then
+    assertThat(credentials).isEqualTo(object);
+  }
 
-    @Test
-    void givenPassword_whenEncrypt_thenSuccess()
+  @Test
+  void givenPassword_whenEncrypt_thenSuccess()
         throws InvalidKeySpecException, NoSuchAlgorithmException, IllegalBlockSizeException,
         InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
-        // given
-        String plainText = "anitalavalatina";
-        String password = "mypass";
-        String salt = "12345678";
-        IvParameterSpec ivParameterSpec = AESUtils.generateIv();
-        SecretKey key = AESUtils.getKeyFromPassword(password, salt);
+    // given
+    String plainText = "anitalavalatina";
+    String password = "mypass";
+    String salt = "12345678";
+    IvParameterSpec ivParameterSpec = AESUtils.generateIv();
+    SecretKey key = AESUtils.getKeyFromPassword(password, salt);
 
-        // when
-        String cipherText = AESUtils.encryptPasswordBased(plainText, key, ivParameterSpec);
-        String decryptedCipherText = AESUtils.decryptPasswordBased(cipherText, key, ivParameterSpec);
+    // when
+    String cipherText = AESUtils.encryptPasswordBased(plainText, key, ivParameterSpec);
+    String decryptedCipherText = AESUtils.decryptPasswordBased(cipherText, key, ivParameterSpec);
 
-        // then
-        Assertions.assertEquals(plainText, decryptedCipherText);
-    }
+    // then
+    Assertions.assertEquals(plainText, decryptedCipherText);
+  }
 }
