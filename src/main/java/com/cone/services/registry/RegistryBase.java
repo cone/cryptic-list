99121dfd007f94302eeb7cfd6fa02f142b1681d3
Carlos Gutierrez <coneramu@gmail.com>
2022-02-07 15:52:35 -0600

    Refactor RegistryCreator service

---
+++ src/main/java/com/cone/services/registry/RegistryBase.java
@@ -0,0 +1,35 @@
+package com.cone.services.registry;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.spec.IvParameterSpec;
+
+import com.cone.services.jsonfile.JsonFileReader;
+
+public abstract class RegistryBase {
+  File file;
+  public final String SALT = "12345678";
+  Map<String, String> entries;
+  String password;
+
+  protected IvParameterSpec getIV() {
+    byte[] iv = { 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8 };
+    return new IvParameterSpec(iv);
+  }
+
+  protected void loadEntries() {
+    try {
+      entries = readFile(file);
+    } catch(IOException error) {
+      entries = new HashMap<>();
+    }
+  }
+
+  protected Map<String, String> readFile(File jsonFile) throws IOException{
+    JsonFileReader fileReader = new JsonFileReader(jsonFile);
+    return fileReader.read();
+  }
+}
--- src/main/java/com/cone/services/registry/RegistryCreator.java
+++ src/main/java/com/cone/services/registry/RegistryCreator.java
@@ -1,8 +1,6 @@
 package com.cone.services.registry;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.crypto.spec.IvParameterSpec;
@@ -10,14 +8,9 @@ import javax.crypto.spec.IvParameterSpec;
 import com.cone.app.Credentials;
 import com.cone.services.credentials.CredentialsEncrypter;
 import com.cone.services.jsonfile.JsonFileCreator;
-import com.cone.services.jsonfile.JsonFileReader;
 import com.cone.services.utils.IdGenerator;
 
-public class RegistryCreator {
-  File file;
-  private final String salt = "12345678";
-  Map<String, String> entries;
-  String password;
+public class RegistryCreator extends RegistryBase {
   IdGenerator idGenerator;
 
   public RegistryCreator(File file, String password, IdGenerator idGenerator) {
@@ -27,11 +20,7 @@ public class RegistryCreator {
   }
 
   public void write(Credentials creds, String description) throws Exception {
-    try {
-      entries = readFile(file);
-    } catch(IOException error) {
-      entries = new HashMap<>();
-    }
+    loadEntries();
 
     String outputFileName = getUniqueId();
     File outputFile = new File(outputFileName);
@@ -41,23 +30,13 @@ public class RegistryCreator {
     writeFile(file, entries);
   }
 
-  private Map<String, String> readFile(File jsonFile) throws IOException{
-    JsonFileReader fileReader = new JsonFileReader(jsonFile);
-    return fileReader.read();
-  }
-
   private String getUniqueId() {
     return idGenerator.createNewId();
   }
 
-  private IvParameterSpec getIV() {
-    byte[] iv = { 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8 };
-    return new IvParameterSpec(iv);
-  }
-
   private void encryptEntry(File outputFile, Credentials creds) throws Exception {
     IvParameterSpec iv = getIV();
-    CredentialsEncrypter encrypter = new CredentialsEncrypter(password, salt, iv, outputFile);
+    CredentialsEncrypter encrypter = new CredentialsEncrypter(password, SALT, iv, outputFile);
     encrypter.write(creds);
   }
 
